d29f7d5bd7132b62b55c2cb136f5e42c6576c359
Vasileios Panopoulos <vpan@kth.se>
2015-05-18 19:35:21 +0200

    initial commit with basic db connectivity working

---
+++ gq.panop.mariadb/src/gq/panop/mariadb/DBConnection.java
@@ -0,0 +1,100 @@
+package gq.panop.mariadb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.Date;
+
+public class DBConnection {
+	
+	private Connection connect = null;
+	private Statement statement = null;
+	private PreparedStatement preparedStatement = null;
+	private ResultSet resultSet = null;
+	
+	public void readDatabase() throws Exception {
+		try {
+			//load the SQL driver
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			//setup the connection 
+			connect = DriverManager.getConnection("jdbc:mysql://192.168.15.202/javatest?" + "user=vasilisp&password=a1b2c3456");
+			
+			//statements to issue queries to the database
+			statement = connect.createStatement();
+			
+			//get results in the result set
+			resultSet = statement.executeQuery("show databases");
+			
+			writeResultSet(resultSet);
+			
+			//PreparedStatements can use variables and are faster
+			//TODO to be filled later
+			
+			writeMetaData(resultSet);
+			
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			close();
+		}
+	}
+		
+	private void writeMetaData(ResultSet resultSet) throws SQLException {
+		
+		//get some general results from the table
+		System.out.println("The columns in the table are: ");
+		
+		System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
+		for (int i =1; i<= resultSet.getMetaData().getColumnCount(); i++){
+			System.out.println("Column " + i + " " + resultSet.getMetaData().getColumnName(i));
+		}
+	}
+	
+	
+	private void writeResultSet(ResultSet resultSet) throws SQLException {
+		while (resultSet.next()){
+			for (int i=1; i<=resultSet.getMetaData().getColumnCount(); i++){
+				System.out.print(resultSet.getObject(i).toString() + " //   ");
+			}
+			System.out.println("");
+		}
+	}
+	
+	private void writeResultSet__original(ResultSet resultSet) throws SQLException {
+		//The cursor of the resultSet is initially before the first data set --> next()
+		
+		while (resultSet.next()) {
+			//get columns either via name or via number, first column starts at 1
+			String user = resultSet.getString("MYUSER");
+			String webpage = resultSet.getString("webpage");
+			String email = resultSet.getString("email");
+			
+			Date date = resultSet.getDate("datum");
+			String comment = resultSet.getString("comments");
+			
+			System.out.println("user : " + user + " || webpage: " + webpage + "  || comment: " + comment);
+		}
+	}
+	
+	private void close(){
+		try{
+			if (resultSet != null) {
+				resultSet.close();
+			}
+			if (statement != null){
+				statement.close();
+			}
+			if (connect != null) {
+				connect.close();
+			}
+		} catch (Exception e){
+
+		}
+	}
+
+}
---
+++ gq.panop.mariadb/src/gq/panop/mariadb/Main.java
@@ -0,0 +1,12 @@
+package gq.panop.mariadb;
+
+import gq.panop.mariadb.DBConnection;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		DBConnection connect = new DBConnection();
+		connect.readDatabase();
+	}
+
+}
